a226ad5253d46cc6d871e699dab00af8ba43d533
Muhammad Maawaz. <maawazali111@gmail.com>
2025-08-04 14:40:33 +0500

    Shifted to Faiss DB

--- app/db.py
+++ app/db.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-import chromadb
 from sqlalchemy.orm import sessionmaker,Session
 from typing import Annotated
 from fastapi import Depends
